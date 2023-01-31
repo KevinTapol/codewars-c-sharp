14b11dab1272a3b73000bba4c82d8eef3e06750f
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-01-31 10:54:44 -0600

    bool to string implicit return using truthy/falsy ternary switch case if else

--- codewars005.cs
+++ codewars005.cs
@@ -40,3 +40,69 @@ public static class Kata
 {
   public static string boolToWord(bool word) => word ? "Yes": "No";
 }
+
+// declaring variables
+public static class Kata
+{
+  static string BOOL_FALSE = "No";
+  static string BOOL_TRUE = "Yes";
+
+  public static string boolToWord(bool word)
+  {
+    return word ? BOOL_TRUE : BOOL_FALSE;
+  }
+}
+
+// switch case
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    //TODO
+    string result = "";
+    switch (word)
+    {
+      case true:
+          result = "Yes";
+          break;
+      case false:
+          result = "No";
+          break;
+    }
+    return result;
+  }
+}
+
+//  I didn't know for Codewars in C# you can use this format
+class Kata
+{
+  public static string boolToWord(bool w) => w ? "Yes" : "No";
+}
+
+// return no unless the if statement is met
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {    
+      if (word) { return "Yes"; }
+      return "No";
+  }
+  
+}
+
+// multiple conditions
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if (word==true)
+    {
+      return "Yes";
+    }
+    else if (word==false)
+    {
+      return "No";
+    }
+    return "";
+  }
+}
